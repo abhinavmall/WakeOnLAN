c303f034b0322053ec574d08e2d8fd72c7b51960
Abhinav Mall <abhinav.0.mall@gmail.com>
2017-11-08 11:30:36 +0530

    wake

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController');
 
-router.get('/computer/on', mainController.turnOnComputer);
-router.post('/computer/on', mainController.turnOnComputer);
+router.get('/computer/on', mainController.wakeComputer);
+router.post('/computer/on', mainController.wakeComputer);
 
 module.exports = router;
