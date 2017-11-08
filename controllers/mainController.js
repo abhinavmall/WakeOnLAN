5f4ee227352c816f07815e4b0e59300b3f72ba39
Abhinav Mall <abhinav.0.mall@gmail.com>
2017-11-08 11:50:09 +0530

    intel

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,13 +1,20 @@
 const wol = require('node-wol');
 
 exports.wakeComputer = (req, res) => {
-  wol.wake(process.env.MAC_ADDRESS, function(error) {
+  wol.wake(process.env.MAC_ADDRESS_KILLER, function(error) {
     if (error) {
       // handle error
       console.log(new Date() + ' Unable to wake computer');
-      res.send({"error": "Unable to wake computer"});
     }
     console.log(new Date() + ' Computer is awake');
-    res.send({"Success": "Computer is awake"});
   });
+  wol.wake(process.env.MAC_ADDRESS_INTEL, function(error) {
+    if (error) {
+      // handle error
+      console.log(new Date() + ' Unable to wake computer');
+      //res.send({"error": "Unable to wake computer"});
+    }
+    console.log(new Date() + ' Computer is awake');
+  });
+  res.send({"Success": "Computer is awake"});
 }
