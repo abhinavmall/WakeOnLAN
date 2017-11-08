13c11e08a874f526ff35c8d4770d730240d50021
Abhinav Mall <abhinav.0.mall@gmail.com>
2017-11-08 10:31:40 +0530

    log

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -4,8 +4,10 @@ exports.turnOnComputer = (req, res) => {
   wol.wake(process.env.MAC_ADDRESS, function(error) {
     if (error) {
       // handle error
+      console.log(new Date() + ' Unable to turn on computer');
       res.send({"error": "Unable to turn on computer."});
     }
+    console.log(new Date() + ' Computer turned on');
     res.send({"Success": "Computer turned on."});
   });
 }
